f82ca102b6f2c3286554b1b82d8d24291280d910
Devanshu Tiwaskar <devanshutiwaskar@gmail.com>
2025-10-26 22:09:01 +0530

    feat: Add emoji to notification server startup message

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ import app from './src/app.js'
 const PORT = process.env.PORT || 4001
 
 app.listen(PORT,()=>{
-    console.log(`Notification server is running on port: ${PORT}`)
+    console.log(`🔥Notification server is running on port: ${PORT}`)
 })
